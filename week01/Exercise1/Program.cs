501321c20fe87d27d8651ba8094ec50654e06ea9
Rufaro Chirume <ruffych594@gmail.com>
2026-09-05 12:05:07 +0200

    input and out completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // This is a code that asks the user for their name.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine(); // This line reads the user's input and stores it in the variable 'firstName'.
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine(); // This line reads the user's input and stores it in the variable 'lastName'.
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}."); // This line outputs a greeting message that includes the user's full name.
     }
 }
